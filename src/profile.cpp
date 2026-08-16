692ecedd37ed2bc26b33881b126d8797ad650a87
Ajay Singh Jhala <ajaysinghjhala007@gmail.com>
2026-08-16 16:22:23 +0530

    added isvalid age logic to verify user age

--- src/profile.cpp
+++ src/profile.cpp
@@ -3,15 +3,27 @@
 
 using namespace std;
 
-display profile(string name, string email){
+void display_profile(string name, string email){
 
   cout<<name;
   cout<<email;
 }
 
+void isvalidage(int age){
+    
+    if(age >= 18){
+    cout<<"valid age";
+    }
+    else{
+    cout<<"invalid age";
+    }
+
+}
+
 int main(){
 string name = "Ajay";
 string email = "xyz@gmail.com";
+int age = 19;
 }
 
 
