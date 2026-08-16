e942af7c15153f5f9555f2ef86aa444bf7659afd
Ajay Singh Jhala <ajaysinghjhala007@gmail.com>
2026-08-16 16:18:08 +0530

    added function display profile

--- src/profile.cpp
+++ src/profile.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 
+display profile(string name, string email){
+
+  cout<<name;
+  cout<<email;
+}
+
 int main(){
 string name = "Ajay";
 string email = "xyz@gmail.com";
 }
+
+
